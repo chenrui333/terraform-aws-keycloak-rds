0f4fa840a042dcdb1ab77f126836f90625fe46ef
Rui Chen <rui@meetup.com>
2020-08-03 17:41:21 -0400

    fix tf plan issue

--- dns/main.tf
+++ dns/main.tf
@@ -17,14 +17,25 @@ resource "aws_acm_certificate" "main" {
 }
 
 resource "aws_route53_record" "validation" {
-  name    = "${aws_acm_certificate.main.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.main.domain_validation_options.0.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.main.zone_id}"
-  records = ["${aws_acm_certificate.main.domain_validation_options.0.resource_record_value}"]
-  ttl     = "60"
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+      name    = dvo.resource_record_name
+      record  = dvo.resource_record_value
+      type    = dvo.resource_record_type
+      zone_id = "${data.aws_route53_zone.main.zone_id}"
+      ttl = "60"
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = each.value.zone_id
 }
 
 resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = "${aws_acm_certificate.main.arn}"
-  validation_record_fqdns = "${aws_route53_record.validation.*.fqdn}"
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
 }
--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "key_name" {
 }
 
 variable "docker_image_url" {
-  default = "jboss/keycloak:9.0.3"
+  default = "jboss/keycloak:11.0.0"
 }
 
 variable "ecs_cluster_name" {
@@ -22,21 +22,25 @@ variable "ecs_log_level" {
 }
 
 variable "admin_cidr_ingress" {
-
+  default = "10.15.0.0/16"
 }
 
 variable "keycloak_admin_username" {
   description = "KeyCloak Admin Username"
+  default = "rg"
 }
 
 variable "keycloak_admin_password" {
   description = "KeyCloak Admin Password"
+  default = "rgdemo1"
 }
 
 variable "public_dns_name" {
   description = "The public-facing DNS name"
+  default = "keycloak.meetuphq.io"
 }
 
 variable "zone_name" {
   description = "The DNS zone name"
+  default = "meetuphq.io"
 }
