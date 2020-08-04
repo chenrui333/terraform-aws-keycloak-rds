7fa740efbe6b59578130f481ca1df246fb89dbc8
Rui Chen <rui@meetup.com>
2020-08-04 16:12:56 -0400

    aws_alb_* known as aws_lb_*

--- alb/main.tf
+++ alb/main.tf
@@ -1,18 +1,18 @@
-resource "aws_alb_target_group" "main" {
+resource "aws_lb_target_group" "main" {
   name     = "${var.alb_target_group_name}"
   port     = "${var.alb_target_port}"
   protocol = "HTTP"
   vpc_id   = "${var.vpc_id}"
 }
 
-resource "aws_alb" "main" {
+resource "aws_lb" "main" {
   name            = "${var.alb_name}"
   subnets         = "${var.subnet_ids}"
   security_groups = "${var.security_groups}"
 }
 
-resource "aws_alb_listener" "front_end_tls" {
-  load_balancer_arn = aws_alb.main.arn
+resource "aws_lb_listener" "front_end_tls" {
+  load_balancer_arn = aws_lb.main.arn
   port              = "443"
   protocol          = "HTTPS"
 
@@ -20,7 +20,7 @@ resource "aws_alb_listener" "front_end_tls" {
   certificate_arn = var.certificate_arn
 
   default_action {
-    target_group_arn = aws_alb_target_group.main.arn
+    target_group_arn = aws_lb_target_group.main.arn
     type             = "forward"
   }
 }
